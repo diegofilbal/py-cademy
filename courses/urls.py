2e39ed057556bd84f3781dedb8c09d1527a867cd
Diego Filgueiras <diego.filgueiras.084@ufrn.edu.br>
2024-05-31 18:47:48 -0300

    feature: implement rate search by course

--- courses/urls.py
+++ courses/urls.py
@@ -4,6 +4,12 @@ from .views import CoursesAPIView, CourseAPIView, RatesAPIView, RateAPIView
 urlpatterns = [
     path("courses/", CoursesAPIView.as_view(), name="courses"),
     path("courses/<int:pk>/", CourseAPIView.as_view(), name="course"),
+    path("courses/<int:course_pk>/rates/", RatesAPIView.as_view(), name="course_rates"),
+    path(
+        "courses/<int:course_pk>/rates/<int:rate_pk>/",
+        RateAPIView.as_view(),
+        name="course_rate",
+    ),
     path("rates/", RatesAPIView.as_view(), name="rates"),
     path("rates/<int:pk>/", RateAPIView.as_view(), name="rate"),
 ]
--- courses/views.py
+++ courses/views.py
@@ -1,6 +1,10 @@
-from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
 from .models import Course, Rate
 from .serializers import CourseSerializer, RateSerializer
+from rest_framework.generics import (
+    ListCreateAPIView,
+    RetrieveUpdateDestroyAPIView,
+    get_object_or_404,
+)
 
 
 class CoursesAPIView(ListCreateAPIView):
@@ -17,7 +21,21 @@ class RatesAPIView(ListCreateAPIView):
     queryset = Rate.objects.all()
     serializer_class = RateSerializer
 
+    def get_queryset(self):
+        if self.kwargs.get("course_pk"):
+            return self.queryset.filter(course_id=self.kwargs.get("course_pk"))
+        return self.queryset.objects.all()
+
 
 class RateAPIView(RetrieveUpdateDestroyAPIView):
     queryset = Rate.objects.all()
     serializer_class = RateSerializer
+
+    def get_object(self):
+        if self.kwargs.get("course_pk"):
+            return get_object_or_404(
+                self.get_queryset(),
+                course_id=self.kwargs.get("course_pk"),
+                pk=self.kwargs.get("rate_pk"),
+            )
+        return get_object_or_404(self.get_queryset(), pk=self.kwargs.get("pk"))
