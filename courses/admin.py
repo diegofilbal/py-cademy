9541ccf821029626bab25d35e23153f5b12139c0
Diego Filgueiras <diego.filgueiras.084@ufrn.edu.br>
2024-05-31 09:14:12 -0300

    feature: implement course and rate models

--- courses/admin.py
+++ courses/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
+from .models import Course, Rate
 
-# Register your models here.
+
+@admin.register(Course)
+class CourseAdmin(admin.ModelAdmin):
+    list_display = ["title", "url", "created_at", "updated_at", "active"]
+
+
+@admin.register(Rate)
+class RateAdmin(admin.ModelAdmin):
+    list_display = [
+        "course",
+        "name",
+        "email",
+        "rate",
+        "created_at",
+        "updated_at",
+        "active",
+    ]
