dff2fded9a10261565de4ac7720b4cb10e295eb2
Diego Filgueiras <diego.filgueiras.084@ufrn.edu.br>
2024-05-31 17:44:54 -0300

    feature: implement basic post request endpoints

--- courses/views.py
+++ courses/views.py
@@ -11,9 +11,23 @@ class CourseApiView(APIView):
         serializer = CourseSerializer(courses, many=True)
         return Response(serializer.data)
 
+    def post(self, request):
+        serializer = CourseSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
 
 class RateApiView(APIView):
     def get(self, request):
         rates = Rate.objects.all()
         serializer = RateSerializer(rates, many=True)
         return Response(serializer.data)
+
+    def post(self, request):
+        serializer = RateSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
